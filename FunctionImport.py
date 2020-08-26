397290d79019c250bf3fce2f4aab87874c9b1b39
Inbell1s <50827900+Inbell1s@users.noreply.github.com>
2020-08-26 17:03:37 -0400

    Add files via upload

---
+++ FunctionImport.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+#
+#
+
+import SnippetsPython.Menu as file ##importing file.py as file basepath.filename
+
+file.menu() #file.function to call on a function
+
+# from basepath.filename import somefunc - to import 1 function only
