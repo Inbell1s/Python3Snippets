b369b253bb1f610cc90bf656211cd935675839a0
Inbell1s <50827900+Inbell1s@users.noreply.github.com>
2020-08-26 16:35:27 -0400

    Added TextColor.py
    Text coloring functions.

---
+++ TextColor.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+#
+#Coloured text function snippet
+#
+
+
+import os
+import sys
+
+#Color Definitions.
+#(Style;Color;Background)
+
+def printRedB(text):
+    print("\x1b[1;31;40m", text, "\x1b[0m", end="", sep="")
+
+def printGreenB(text):
+    print("\x1b[1;32;40m", text, "\x1b[0m", end="", sep="")
+
+def printYellowB(text):
+    print("\x1b[1;33;40m", text, "\x1b[0m", end="", sep="")
+
+#Usage.
+printRedB("test\n")
+
+#Can print variables too.
+tekst = "tester\n"
+printGreenB(tekst)
+
+#Print on one line
+print("Hallo, mag ik ", end="")
+printYellowB("kleur")
+print(" astublieft")
